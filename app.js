7375d4c697be261007c14dfac042de7d271d80a4
kan dhillon <kdhillon469@gmail.com>
2025-04-27 19:51:26 +1000

    add menubar feature

---
+++ app.js
@@ -0,0 +1,9 @@
+function openMenu() {
+    document.querySelector('.nav').classList.add('open')
+    // document.body.classList.add('open')
+}
+
+function closeMenu() {
+    document.querySelector('.nav').classList.remove('open')
+    // document.body.classList.remove('open')
+}
